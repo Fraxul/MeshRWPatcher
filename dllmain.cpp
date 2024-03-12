227a647fe374cd3f1f59d124ae3ab17a46c7a965
Dan Weatherford <sabretooth@gmail.com>
2024-03-11 23:28:06 -0500

    Avoid touching meshes inside of scene files saved during build

--- dllmain.cpp
+++ dllmain.cpp
@@ -54,6 +54,23 @@ static const uint8_t* memmem(const uint8_t* big, size_t big_length, const uint8_
  */
 
 
+/*
+ * Callstack for saving meshes inside of a scene file during the build:
+ * (we avoid modifying meshes saved like this by adding a guard around BackupScenes)
+ *
+ *	Unity.exe!Mesh::Transfer<class StreamedBinaryWrite>(class StreamedBinaryWrite &)
+ * 	Unity.exe!SerializedFile::WriteObject(class Object &,__int64,short,struct BuildUsageTag const &,struct GlobalBuildData const &)
+ *	Unity.exe!PersistentManager::WriteFile(class core::basic_string_ref<char>,int,struct WriteData const *,int,struct GlobalBuildData const &,enum VerifyWriteObjectResult (*)(class Object *,enum BuildTargetPlatform),struct BuildTargetSelection,enum TransferInstructionFlags,struct WriteInformation &,struct InstanceIDResolver const *,enum PersistentManager::LockFlags,int (*)(enum PersistentManager::ReportWriteObjectStep,int,class core::basic_string<char,class core::StringStorageDefault<char> > const &,int,void *),void *)
+ *	Unity.exe!PersistentManager::WriteFile(class core::basic_string_ref<char>,int,struct WriteData const *,int,struct GlobalBuildData const &,enum VerifyWriteObjectResult (*)(class Object *,enum BuildTargetPlatform),struct BuildTargetSelection,enum TransferInstructionFlags,struct InstanceIDResolver const *,enum PersistentManager::LockFlags,int (*)(enum PersistentManager::ReportWriteObjectStep,int,class core::basic_string<char,class core::StringStorageDefault<char> > const &,int,void *),void *)
+ *	Unity.exe!SaveScene(class UnityScene const &,class core::basic_string<char,class core::StringStorageDefault<char> > const &,class core::hash_map<__int64,int,struct core::hash<__int64>,struct std::equal_to<__int64> > *,enum TransferInstructionFlags)
+ *	Unity.exe!EditorSceneManager::SerializeSceneIntoFile(class UnityScene &,class core::basic_string<char,class core::StringStorageDefault<char> > const &,enum TransferInstructionFlags,class core::hash_map<__int64,int,struct core::hash<__int64>,struct std::equal_to<__int64> > *)
+ *	Unity.exe!EditorSceneManager::BackupScenesFromSceneManager(bool,struct dynamic_array<struct EditorSceneBackup,0> &)
+ *  Unity.exe!BackupScenes(struct dynamic_array<struct EditorSceneBackup,0> & __ptr64) XXX this call goes here, but it gets tail-call optimized out so it doesn't appear in the backtrace.
+ *	Unity.exe!BuildAssetBundlesInternal()
+ *	Unity.exe!BuildAssetBundles(class core::basic_string<char,class core::StringStorageDefault<char> > const &,struct dynamic_array<struct AssetBundleBuildInfo,0> const &,enum BuildTargetPlatformGroup,enum BuildTargetPlatform,int,enum BuildAssetBundleOptions,bool,class BuildReporting::BuildReport &)
+ *	Unity.exe!BuildPipeline_CUSTOM_BuildAssetBundlesWithInfoInternal(class ScriptingBackendNativeStringPtrOpaque *,class ScriptingBackendNativeArrayPtrOpaque *,enum BuildAssetBundleOptions,enum BuildTargetPlatformGroup,enum BuildTargetPlatform,int)
+*/
+
 
 // All signatures are for Unity 2022.3.6f1
 // Unity.exe!Mesh::Transfer<class StreamedBinaryWrite>(class StreamedBinaryWrite &)
@@ -64,13 +81,18 @@ const uint8_t Mesh_Transfer_signature[] = { 0x40, 0x55, 0x53, 0x56, 0x57, 0x41, 
 const uint8_t IsBuildingPlayer_signature[] = { 0x0f, 0xb6, 0x05, 0xb1, 0x08, 0xd1, 0x01, 0xc3 };
 static bool(__cdecl* IsBuildingPlayer)(void);
 
+// void __cdecl BackupScenes(struct dynamic_array<struct EditorSceneBackup,0> & __ptr64)
+const uint8_t BackupScenes_signature[] = { 0x40, 0x53, 0x48, 0x83, 0xec, 0x20, 0x48, 0x8b, 0xd9, 0xe8, 0x82, 0x59, 0x8a, 0xff, 0xe8, 0x3d, 0x38, 0x45, 0xff };
+static void (__cdecl *original_BackupScenes)(void* param1);
+static bool s_inBackupScenes = false;
+
+
 
 void __cdecl replacement_Mesh_Transfer__class_StreamedBinaryWriter(void* this_Mesh, void* streamedBinaryWriter) {
-    if (!IsBuildingPlayer()) {
-        // Don't do anything if we're not building the player right now
+    if (!IsBuildingPlayer() || s_inBackupScenes) {
+        // Don't do anything if we're not building the player right now, or if we're saving a scene through BackupScenes()
         (*original_Mesh_Transfer__class_StreamedBinaryWriter)(this_Mesh, streamedBinaryWriter);
     }
-
 
     //debugPrintf("MeshRWPatcher: replacement Mesh::Transfer: this=%p streamedBinaryWriter=%p", this_Mesh, streamedBinaryWriter);
     uint8_t* pIsReadable = reinterpret_cast<uint8_t*>(this_Mesh) + 0x161; // m_IsReadable offset in Mesh class
@@ -78,6 +100,12 @@ void __cdecl replacement_Mesh_Transfer__class_StreamedBinaryWriter(void* this_Me
     *pIsReadable = 0;
     (*original_Mesh_Transfer__class_StreamedBinaryWriter)(this_Mesh, streamedBinaryWriter);
     *pIsReadable = old_IsReadable;
+}
+
+void __cdecl replacement_BackupScenes(void* param1) {
+    s_inBackupScenes = true;
+    original_BackupScenes(param1);
+    s_inBackupScenes = false;
 }
 
 
@@ -119,9 +147,20 @@ BOOL APIENTRY DllMain( HMODULE hModule, DWORD  dwReason, LPVOID lpReserved) {
             IsBuildingPlayer = reinterpret_cast<bool(__cdecl*)()>(fn_base);
         }
 
+        {
+            const uint8_t* fn_base = memmem((const uint8_t*) moduleInfo.lpBaseOfDll, moduleInfo.SizeOfImage, BackupScenes_signature, sizeof(BackupScenes_signature));
+            if (!fn_base) {
+                OutputDebugString(L"MeshRWPatcher: Can't find the BackupScenes function!");
+                return TRUE;
+            }
+            debugPrintf("MeshRWPatcher: BackupScenes() function is at 0x%p", fn_base);
+            original_BackupScenes = reinterpret_cast<void(__cdecl*)(void*)>(fn_base);
+        }
+
         DetourTransactionBegin();
         DetourUpdateThread(GetCurrentThread());
         DetourAttach(&(PVOID&) original_Mesh_Transfer__class_StreamedBinaryWriter, replacement_Mesh_Transfer__class_StreamedBinaryWriter);
+        DetourAttach(&(PVOID&) original_BackupScenes, replacement_BackupScenes);
         DetourTransactionCommit();
         
     } else if (dwReason == DLL_PROCESS_DETACH) {
@@ -129,6 +168,7 @@ BOOL APIENTRY DllMain( HMODULE hModule, DWORD  dwReason, LPVOID lpReserved) {
         DetourTransactionBegin();
         DetourUpdateThread(GetCurrentThread());
         DetourDetach(&(PVOID&) original_Mesh_Transfer__class_StreamedBinaryWriter, replacement_Mesh_Transfer__class_StreamedBinaryWriter);
+        DetourDetach(&(PVOID&) original_BackupScenes, replacement_BackupScenes);
         DetourTransactionCommit();
     }
 
